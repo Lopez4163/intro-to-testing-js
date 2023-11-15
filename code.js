c29f36fb81c6f73f70ed19ab58c2682cd6ca04f8
Nicholas Lopez <Lopez4163@gmail.com>
2023-11-15 16:43:33 -0600

    final push all complete

--- code.js
+++ code.js
@@ -3,6 +3,23 @@ function helloWorld() {
     return "Hello, World!"
 }
 
-const sayHello = () => {
-    return 'yep'
+const sayHello = (x) => {
+    if(x === true || x === false) {
+        return "Hello, World!"
+    }
+
+    else{
+        return `Hello, ${x}!`
+    }
+
 }
+
+console.log(sayHello("Alex"))
+
+// const sayHello = (x) => { return `Hello, ${x}!`}
+
+sayHello('Jane')
+sayHello('Alex')
+sayHello('Pat')
+sayHello('World')
+
